fbef94a5f5e565466a0f7ad08a27f5df6b5d376e
Abdallah Abdirashid <162161971+AbdulahiAwil@users.noreply.github.com>
2024-10-30 22:44:50 +0300

    Update
    commit kan laguma guuleysan

--- menu.js
+++ menu.js
@@ -1,21 +1,32 @@
 const hamburger = document.querySelector(".hamburger");
-    const mobileMenu = document.querySelector(".menu-link");
-    const icons = document.querySelectorAll("i");
-    console.log('hamburger',hamburger);
-    hamburger.addEventListener("click", function (event) {
-console.log('event :',event)
-        const isVisible = mobileMenu.getAttribute('data-visible');
-        
-        if (isVisible == "true") {
+const mobileMenu = document.querySelector(".menu-link");
+const icons = document.querySelectorAll("i");
+;
+console.log('hamburger', hamburger);
+hamburger.addEventListener("click", function (event) {
+    console.log('event :', event)
+    const isVisible = mobileMenu.getAttribute('data-visible');
 
-            // qari hiding
-            mobileMenu.setAttribute('data-visible', "false");
-            icons[0].setAttribute('data-visible', "true");
-            icons[1].setAttribute('data-visible', "false");
-        } else {
-            // soo bandhig showing
-            mobileMenu.setAttribute('data-visible', "true");
-            icons[0].setAttribute('data-visible', "false");
-            icons[1].setAttribute('data-visible', "true");
-        }
-    });
+    if (isVisible == "true") {
+
+        // qari hiding
+        mobileMenu.setAttribute('data-visible', "false");
+        icons[0].setAttribute('data-visible', "true");
+        icons[1].setAttribute('data-visible', "false");
+    
+        
+
+        
+
+    } else {
+        // soo bandhig showing
+        mobileMenu.setAttribute('data-visible', "true");
+        icons[0].setAttribute('data-visible', "false");
+        icons[1].setAttribute('data-visible', "true");
+        
+    }
+});
+
+
+
+
